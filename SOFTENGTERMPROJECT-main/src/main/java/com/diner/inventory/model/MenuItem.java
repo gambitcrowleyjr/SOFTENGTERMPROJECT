55538ca7b0429d032ba9ea9276898ce7db0e38c1
SajjadHaider451 <149915948+SajjadHaider451@users.noreply.github.com>
2026-04-02 18:40:28 -0400

    Manager edit menu

---
+++ SOFTENGTERMPROJECT-main/src/main/java/com/diner/inventory/model/MenuItem.java
@@ -0,0 +1,25 @@
+package com.diner.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    @OneToMany(mappedBy = "menuItem", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MenuItemIngredient> ingredients = new ArrayList<>();
+}
